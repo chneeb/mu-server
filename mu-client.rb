cb4acd841d19f1779a73bdeb7a4eae2e935113e6
Georg Kreimer <georgkreimer@kallisto.local>
2009-12-21 17:06:48 -0500

    indexing removed

--- mu-client.rb
+++ mu-client.rb
@@ -5,6 +5,5 @@ require 'drb'
 
 DRb.start_service
 mu_server = DRbObject.new(nil, 'druby://10.201.55.35:8001')
-mu_server.index("neeb03")
 mail = mu_server.find("neeb03", ARGV.join).first
 puts mu_server.retrieve_mail("neeb03", mail["mpath"])
